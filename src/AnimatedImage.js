c9e7cfbd79a84bab0f189942c250881e7f1878ad
Nicolas Debiaggi <nicolas.debiaggi@globant.com>
2023-02-17 18:30:25 -0300

    css animation added

--- src/AnimatedImage.js
+++ src/AnimatedImage.js
@@ -1,38 +1,18 @@
-import { useState } from 'react'
-import { m } from 'framer-motion'
 import DogeImage from './doge-min.png'
 
-const AnimatedImage = ({DEF_XSPEED, DEF_YSPEED, IMG_SIZE}) => {
-  const [x, setX] = useState(Math.random() * window.innerWidth/2 - IMG_SIZE)
-  const [speedX, setSpeedX] = useState(Math.random() > 0.5 ? DEF_XSPEED : -DEF_XSPEED)
-  const [y, setY] = useState(Math.random() * window.innerHeight/2 - IMG_SIZE)
-  const [speedY, setSpeedY] = useState(Math.random() > 0.5 ? DEF_YSPEED : -DEF_YSPEED)
-
-  const handleAnimationComplete = () => {
-    setX(x + speedX);
-    setY(y + speedY);
-    if (x >= window.innerWidth/2 - IMG_SIZE) {
-      setSpeedX(-DEF_XSPEED * Math.random());
-    }
-    if (x <= -window.innerWidth/2) {
-      setSpeedX(DEF_XSPEED * Math.random());
-    }
-    if (y >= window.innerHeight/2 - IMG_SIZE) {
-      setSpeedY(-DEF_YSPEED * Math.random());
-    }
-    if (y <= -window.innerHeight/2) {
-      setSpeedY(DEF_YSPEED * Math.random());
-    }
-  }
-
+const speedX = 2500
+const AnimatedImage = ({IMG_SIZE}) => {
   return (
-    <m.img
+    <img
       src={DogeImage}
-      animate={{ x: x, y: y }}
-      transition={{ ease: "linear" }}
-      onAnimationComplete={handleAnimationComplete}
+      alt='doge'
       className='image'
-      style={{ height: IMG_SIZE, width: IMG_SIZE }}
+      style={{
+        height: IMG_SIZE,
+        width: IMG_SIZE,
+        animationDelay: `${Math.random() * 10}s`,
+        '--randomnessDelta': `${Math.random() > 0.5 ? speedX * Math.random() : speedX * Math.random() * -1}px`
+      }}
     />
   )
 }
