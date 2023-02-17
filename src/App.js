a9e86a72d05a5d85d2887f7b49e6cd1de5185af3
Nicolas Debiaggi <nicolas.debiaggi@globant.com>
2023-02-17 18:35:05 -0300

    css animations added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { LazyMotion, domAnimation } from 'framer-motion'
 import AnimatedImage from './AnimatedImage'
 import './App.css'
 
 const IMG_SIZE = 300
+const DEF_XSPEED = 150
+const DEF_YSPEED = 150
 
 function App() {
   return (
     <div className="app">
-      {[...Array(500)].map((_, i) => (
-        <AnimatedImage key={i} IMG_SIZE={IMG_SIZE} />
-      ))}
+      <LazyMotion features={domAnimation}>
+        {[...Array(500)].map((_, i) => (
+          <AnimatedImage key={i} DEF_XSPEED={DEF_XSPEED} DEF_YSPEED={DEF_YSPEED} IMG_SIZE={IMG_SIZE} />
+        ))}
+      </LazyMotion>
     </div>
   )
 }
